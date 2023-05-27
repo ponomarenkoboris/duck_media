b00e6a3cb421ba469ff392f42bbccaa54ce7ef58
Boris Ponomarenko <borisponomarenko.dmi@gmail.com>
2023-05-27 21:18:31 +0300

    Debug

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -2,7 +2,6 @@ import Layout from '@/components/layout/layout/Layout'
 import styles from './Home.module.scss'
 
 export default function Home() {
-	console.log(styles)
 	return (
 		<Layout>
 			<div className={styles.homepage}>
@@ -10,17 +9,17 @@ export default function Home() {
 					<p className={styles.homepage__intro}>Мы создаем:</p>
 					<div className={styles.homepage__benefits}>
 						<h1 className={styles.benefits__item}>Успех</h1>
-						<h1 className={styles.benefits__item}>Рост</h1>
-						<h1 className={styles.benefits__item}>Прибыль</h1>
-						<h1 className={styles.benefits__item}>Вовлеченность</h1>
-						<h1 className={styles.benefits__item}>Инновации</h1>
-						<h1 className={styles.benefits__item}>Конкурентоспособность</h1>
-						<h1 className={styles.benefits__item}>Репутация</h1>
-						<h1 className={styles.benefits__item}>Лояльность к бренду</h1>
-						<h1 className={styles.benefits__item}>Эффективность</h1>
-						<h1 className={styles.benefits__item}>Качество</h1>
-						<h1 className={styles.benefits__item}>Клиентов</h1>
-						<h1 className={styles.benefits__item}>Безопасность</h1>
+						<p className={styles.benefits__item}>Рост</p>
+						<p className={styles.benefits__item}>Прибыль</p>
+						<p className={styles.benefits__item}>Вовлеченность</p>
+						<p className={styles.benefits__item}>Инновации</p>
+						<p className={styles.benefits__item}>Конкурентоспособность</p>
+						<p className={styles.benefits__item}>Репутация</p>
+						<p className={styles.benefits__item}>Лояльность к бренду</p>
+						<p className={styles.benefits__item}>Эффективность</p>
+						<p className={styles.benefits__item}>Качество</p>
+						<p className={styles.benefits__item}>Клиентов</p>
+						<p className={styles.benefits__item}>Безопасность</p>
 					</div>
 				</div>
 			</div>
