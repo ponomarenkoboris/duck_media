a852f644ac4f31a65c2f96f56a8489ff382afc5d
Boris Ponomarenko <borisponomarenko.dmi@gmail.com>
2023-05-30 20:20:50 +0300

    Добавленна обработка неправильного роута

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@styles/globals.scss";
-import type { AppProps } from "next/app";
+import App, { AppContext } from 'next/app'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+function DuckApp({ Component, pageProps }: AppProps) {
+  	return <Component {...pageProps} />
 }
+
+DuckApp.getInitialProps = async (duckCtx: AppContext) => {
+	const appProps = await App.getInitialProps(duckCtx);
+
+	if (duckCtx.ctx.res?.statusCode === 404) {
+		duckCtx.ctx.res.writeHead(301, { Location: '/' });
+		duckCtx.ctx.res.end();
+	}
+
+  	return appProps;
+};
+
+export default DuckApp
